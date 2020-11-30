81254f87af9504fdb5f75371d1a85bf6a721794b
AndreaG <gaggiandrea@gmail.com>
2020-11-30 17:04:59 +0100

    Some html and js refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,39 +2,37 @@ $(function () {
   
   var slidableTabs = $('li.slidable');
 
+  //mouse sopra le tab della nav right
   slidableTabs.mouseover(function() {
-    slidableTabs.children('ul').css('display', 'none');
-    $(this).children('ul').toggle();
+    //appare il submenu corrispondente al tab
+    slidableTabs.children('.sub_menu').css('display', 'none');
+    $(this).children('.sub_menu').toggle();
+
+    //il "titolo" del tab diventa nero
+    slidableTabs.children('a').removeClass('black');
+    $(this).children('a').addClass('black');
+
+    //effetti sui link dei submenu al mouseover e ripristino stato di default
+    $('.sub_menu a').mouseover(function() {
+      $(this).css({
+        'text-decoration': 'underline',
+        'color': 'black'
+      });
+    });
+    //ripristino al mouseout
+    $('.sub_menu a').mouseout(function() {
+      $(this).css({
+        'text-decoration': 'none',
+        'color': 'darkgrey'
+      });
+    });
   });
 
+  //clicco fuori dalla nav e torna tutto allo stato iniziale
   $(document).click(function() {
-    slidableTabs.children('ul').css('display', 'none');
+    slidableTabs.children('.sub_menu').css('display', 'none');
+    slidableTabs.children('a').removeClass('black');
   })
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
